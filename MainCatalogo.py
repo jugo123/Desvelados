19c231baf6c5d019c2c05b540aa31b0e60c9a9d0
jugo123 <103468623+jugo123@users.noreply.github.com>
2023-12-22 02:15:56 -0300

    cambios minimos

--- MainCatalogo.py
+++ MainCatalogo.py
@@ -1,11 +1,11 @@
 from Presentador import Virus
-from Presentador import Catalogo
+from Presentador import catalogo
 from Modelo import CRUDVirus
 virus1 = Virus.Virus("Severe acute respiratory syndrome coronavirus 2", "SARS-CoV-2", "2019-12-01")
 virus2 = Virus.Virus("Human Immunodeficiency Virus", "HIV", "1983-05-20")
 virus3 = Virus.Virus("Influenza A virus subtype H1N1", "H1N1", "2009-04-15")
 
-cat1= Catalogo.Catalogo(101, "Catálogo 1")
+cat1= catalogo.Catalogo(101, "Catálogo 1")
 CRUDVirus.ingresar(virus1)
 
 cat1.agregar_al_catalogo(virus1)
